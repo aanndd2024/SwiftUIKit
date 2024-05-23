96e570d255e90bbe7bcacd36511fbaca0c3c1e4d
Anand Yadav <anand.yadav@nagarro.com>
2024-05-23 18:59:14 +0530

    CoreData initial commit

---
+++ CoreDataSample/CoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Anand Yadav on 23-05-2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
